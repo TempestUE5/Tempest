ead772f4ab4f42f541ed5a0ac04f48ea902567d6
danieljgerhardt <danieljgerhardt@gmail.com>
2025-11-01 18:25:19 -0400

    boss enemy

--- Source/DrownMeNot/WaveManager.cpp
+++ Source/DrownMeNot/WaveManager.cpp
@@ -45,14 +45,6 @@ void AWaveManager::Tick(float DeltaTime)
 
 void AWaveManager::StartNextWave()
 {
-	//restore player health to full
-	//get player character
-	ACharacterBase* PlayerCharacter = Cast<ACharacterBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
-	//get player attribute set
-	if (PlayerCharacter) {
-		PlayerCharacter->RestoreHealthToFull();
-	}
-
 	CurrentWave++;
 	switch (OverallWaveType)
 	{
