ae024f24b6c10ca13306edc33781efd3f659f19b
Amy Liu <mialana@users.noreply.github.com>
2025-11-02 22:09:44 -0500

    feat: modiy spawn sound

--- Source/DrownMeNot/GameplayAbilitySystem/Characters/CharacterBase.cpp
+++ Source/DrownMeNot/GameplayAbilitySystem/Characters/CharacterBase.cpp
@@ -60,7 +60,7 @@ void ACharacterBase::BeginPlay()
 
     if (!IsPlayerControlled()) {
         if (SpawnSound) {
-            UGameplayStatics::PlaySoundAtLocation(this, SpawnSound, GetActorLocation());
+            UGameplayStatics::PlaySoundAtLocation(this, SpawnSound, GetActorLocation(), 2.f);
         }
 
         if (SpawnEffect) {
